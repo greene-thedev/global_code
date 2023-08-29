81a280eb1f691e16449f5bfd38bebf405d7f2e58
Jeremiah Greene <jerrygreene010@gmail.com>
2023-08-29 13:52:12 +0000

    implemented lesson 7

--- labs/IOT/app.py
+++ labs/IOT/app.py
@@ -1,7 +1,18 @@
+# importing package for making api requests
 import requests
+# importing GPIO packages
+from gpiozero import Button
+from signal import pause
 
-url = "https://maker.ifttt.com/trigger/notification/json/with/key/lxi_09_iTWs-6l0Avj5SsFUOk0X7GuK3mHeVKH_9GbT"
+# making API call a function
+def get_url():
+    url = "https://maker.ifttt.com/trigger/notification/json/with/key/lxi_09_iTWs-6l0Avj5SsFUOk0X7GuK3mHeVKH_9GbT"
+    r = requests.get(url)
+    print(r)
 
-r = requests.get(url)
+# giving the button logic    
+button = Button(2)
+button.when_pressed = get_url
+# button.when_released =
 
-print(r)
+pause()
