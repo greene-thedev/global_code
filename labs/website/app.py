15f432d28b23a5b6fe3ff3aa23ed2fa05a6cff5b
Jeremiah Greene <jerrygreene010@gmail.com>
2023-08-30 10:38:55 +0000

    Html addition

--- labs/website/app.py
+++ labs/website/app.py
@@ -1,11 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello, world!"
+    return render_template('index.html')
+
+# @app.route('/whoami')
+# def whoami():
+#     return 
+    
 
 if __name__ == '__main__':
     app.run(debug=True, host='127.0.0.1')
-    
